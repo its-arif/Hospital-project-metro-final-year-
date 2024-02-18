db6f170b11ec6b203be1a208afc39bf819c0dba3
JaannatulFerdous <85277691+JaannatulFerdous@users.noreply.github.com>
2024-02-18 14:06:20 +0600

    Add files via upload

---
+++ appointment-history.php
@@ -0,0 +1,187 @@
+<?php
+session_start();
+error_reporting(0);
+include('include/config.php');
+if(strlen($_SESSION['id']==0)) {
+ header('location:logout.php');
+  } else{
+
+if(isset($_GET['cancel']))
+		  {
+mysqli_query($con,"update appointment set doctorStatus='0' where id ='".$_GET['id']."'");
+                  $_SESSION['msg']="Appointment canceled !!";
+		  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Doctor | Appointment History</title>
+		
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+	</head>
+	<body>
+	<div class="container navigation" style="background: -webkit-linear-gradient(left, #3931af, #00c6ff);width: 100%;height: 70px;padding-top: 10px;">
+
+
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+          <a class="navbar-brand js-scroll-triggr" href="http://localhost/hospital/hospital/index.php"  style="margin-right: 40px;font-family: 'IBM Plex Sans', sans-serif; font-size: 20px; color: white">METROPOLITAN HOSPITAL</a>
+        </div>
+
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+          <ul class="nav navbar-nav">
+		    <li><a href="http://localhost/hospital/hospital/about.php" style="margin-right: 40px;font-family: 'IBM Plex Sans', sans-serif;color: white">HOME</a></li>
+			<li><a href="http://localhost/hospital/hospital/about.php" style="margin-right: 40px;font-family: 'IBM Plex Sans', sans-serif;color: white">ABOUT US</a></li>
+            <li><a href="http://localhost/hospital/hospital/contact.php" style="margin-right: 40px;font-family: 'IBM Plex Sans', sans-serif;color: white">CONTACT</a></li>
+            <li><a href="http://localhost/hospital/hospital/LOGX.html" style="margin-right: 40px;font-family: 'IBM Plex Sans', sans-serif;color: white">LOG IN</a></li>
+         
+
+              </ul>
+        </div>
+		</div>
+  
+      </div>
+		<div id="app">		
+<?php include('include/sidebar.php');?>
+			<div class="app-content">
+				
+
+					<?php include('include/header.php');?>
+				 
+				<div class="main-contett" style= "background:  white">
+					<div class="wrap-content container" id="container">
+						 
+						<section id="page-title">
+							<div class="row">
+								<div class="col-sm-8">
+									<h1 class="mainTitle">Doctor  | Appointment History</h1>
+																	</div>
+								<ol class="breadcrumb">
+									<li>
+										<span>Doctor </span>
+									</li>
+									<li class="active">
+										<span>Appointment History</span>
+									</li>
+								</ol>
+							</div>
+						</section>
+						 
+						 
+						<div class="container-fluid container-fullw bg-white">
+						
+
+									<div class="row">
+								<div class="col-md-12">
+									
+									<p style="color:red;"><?php echo htmlentities($_SESSION['msg']);?>
+								<?php echo htmlentities($_SESSION['msg']="");?></p>	
+									<table class="table table-hover" id="sample-table-1">
+										<thead>
+											<tr>
+												<th class="center">#</th>
+												<th class="hidden-xs">Patient  Name</th>
+												<th>Specialization</th>
+												<th>Consultancy Fee</th>
+												<th>Appointment Date / Time </th>
+											
+												<th>Current Status</th>
+												<th>Action</th>
+												
+											</tr>
+										</thead>
+										<tbody>
+<?php
+$sql=mysqli_query($con,"select users.fullName as fname,appointment.*  from appointment join users on users.id=appointment.userId where appointment.doctorId='".$_SESSION['id']."'");
+$cnt=1;
+while($row=mysqli_fetch_array($sql))
+{
+?>
+
+											<tr>
+												<td class="center"><?php echo $cnt;?>.</td>
+												<td class="hidden-xs"><?php echo $row['fname'];?></td>
+												<td><?php echo $row['doctorSpecialization'];?></td>
+												<td><?php echo $row['consultancyFees'];?></td>
+												<td><?php echo $row['appointmentDate'];?> / <?php echo
+												 $row['appointmentTime'];?>
+												</td>
+												
+												<td>
+<?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
+{
+	echo "Active";
+}
+if(($row['userStatus']==0) && ($row['doctorStatus']==1))  
+{
+	echo "Cancel by Patient";
+}
+
+if(($row['userStatus']==1) && ($row['doctorStatus']==0))  
+{
+	echo "Cancel by you";
+}
+
+
+
+												?></td>
+												<td >
+												<div class="visible-md visible-lg hidden-sm hidden-xs">
+							<?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
+{ ?>
+
+													
+	<a href="appointment-history.php?id=<?php echo $row['id']?>&cancel=update" onClick="return confirm('Are you sure you want to cancel this appointment ?')"class="btn btn-transparent btn-xs tooltips" title="Cancel Appointment" tooltip-placement="top" tooltip="Remove">Cancel</a>
+	<?php } else {
+
+		echo "Canceled";
+		} ?>
+												</div>
+												</td>
+											</tr>
+											
+											<?php 
+$cnt=$cnt+1;
+											 }?>
+											
+											
+										</tbody>
+									</table>
+								</div>
+							</div>
+								</div>
+						
+						 
+						 
+						
+					</div>
+				</div>
+			</div>
+			 
+		</div>
+	 
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+ 
+	</body>
+</html>
+<?php } ?>
