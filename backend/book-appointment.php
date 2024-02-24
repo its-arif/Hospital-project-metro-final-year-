893af02ae898e7e54781cefdda45afec39314544
its-arif <its.arif736@gmail.com>
2024-02-24 06:04:28 +0600

    Update book-appointment.php

--- backend/book-appointment.php
+++ backend/book-appointment.php
@@ -201,3 +201,93 @@ $query=mysqli_query($con,"insert into appointment(doctorSpecialization,doctorId,
                                   
                                   </ul>
                               </div>
+                              
+                                             
+                                            <div class="main-contet" >
+                                                <div class="wrap-content container" id="container">
+                                                     
+                                                    <section id="page-title">
+                                                        <div class="row">
+                                                            <div class="col-sm-8">
+                                                                <h1 class="mainTitle">User | Book Appointment</h1>
+                                                                                                </div>
+                                                            <ol class="breadcrumb">
+                                                                <li>
+                                                                    <span>User</span>
+                                                                </li>
+                                                                <li class="active">
+                                                                    <span>Book Appointment</span>
+                                                                </li>
+                                                            </ol>
+                                                    </section>
+
+
+                                                    
+                                                                             
+                                                                             
+                                                                            <div class="container-fluid container-fullw bg-white">
+                                                                                <div class="row">
+                                                                                    <div class="col-md-12">
+                                                                                        
+                                                                                        <div class="row margin-top-30">
+                                                                                            <div class="col-lg-8 col-md-12">
+                                                                                                <div class="panel panel-white">
+                                                                                                    <div class="panel-heading">
+                                                                                                        <h5 class="panel-title">Book Appointment</h5>
+                                                                                                    </div>
+                                                                                                    <div class="panel-body">
+                                                                                    <p style="color:red;"><?php echo htmlentities($_SESSION['msg1']);?>
+                                                                                    <?php echo htmlentities($_SESSION['msg1']="");?></p>	
+                                                                                                        <form role="form" name="book" method="post" >
+                                                                                                            
+                                                    
+                                                    
+                                                    <div class="form-group">
+                                                                                                                <label for="DoctorSpecialization">
+                                                                                                                    Doctor Specialization
+                                                                                                                </label>
+
+
+                                                                                                                
+                                                                                                                                            <select name="Doctorspecialization" class="form-control" onChange="getdoctor(this.value);" required="required">
+                                                                                                                                                                                <option value="">Select Specialization</option>
+                                                                                                                <?php $ret=mysqli_query($con,"select * from doctorspecilization");
+                                                                                                                while($row=mysqli_fetch_array($ret))
+                                                                                                                {
+                                                                                                                ?>
+                                                                                                                                                                                <option value="<?php echo htmlentities($row['specilization']);?>">
+                                                                                                                                                                                    <?php echo htmlentities($row['specilization']);?>
+                                                                                                                                                                                </option>
+                                                                                                                                                                                <?php } ?>
+                                                                                                                                                                                
+                                                                                                                                                                            </select>
+                                                                                                                                                                        </div>
+
+                                                                                                                                                                        
+
+
+
+
+														<div class="form-group">
+															<label for="doctor">
+																Doctors
+															</label>
+						<select name="doctor" class="form-control" id="doctor" onChange="getfee(this.value);" required="required">
+						<option value="">Select Doctor</option>
+						</select>
+														</div>
+
+
+
+
+
+														<div class="form-group">
+															<label for="consultancyfees">
+																Consultancy Fees
+															</label>
+					<select name="fees" class="form-control" id="fees"  readonly>
+						
+						</select>
+														</div>
+														
+<div class="form-group">
