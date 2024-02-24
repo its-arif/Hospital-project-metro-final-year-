a7852fe0342f99dbcd2fcb89e32ecde6059710f1
its-arif <its.arif736@gmail.com>
2024-02-24 06:02:01 +0600

    Update book-appointment.php

--- backend/book-appointment.php
+++ backend/book-appointment.php
@@ -7,3 +7,19 @@ check_login();
 
 if(isset($_POST['submit']))
 {
+$specilization=$_POST['Doctorspecialization'];
+$doctorid=$_POST['doctor'];
+$userid=$_SESSION['id'];
+$fees=$_POST['fees'];
+$appdate=$_POST['appdate'];
+$time=$_POST['apptime'];
+$userstatus=1;
+$docstatus=1;
+$query=mysqli_query($con,"insert into appointment(doctorSpecialization,doctorId,userId,consultancyFees,appointmentDate,appointmentTime,userStatus,doctorStatus) values('$specilization','$doctorid','$userid','$fees','$appdate','$time','$userstatus','$docstatus')");
+    if($query)
+    {
+        echo "<script>alert('Your appointment successfully booked');</script>";
+    }
+
+}
+?>
