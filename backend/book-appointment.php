64abcb5f388c4ccc102263f8cb6a82ab1e87c035
its-arif <its.arif736@gmail.com>
2024-02-24 06:06:44 +0600

    Update book-appointment.php

--- backend/book-appointment.php
+++ backend/book-appointment.php
@@ -309,4 +309,74 @@ $query=mysqli_query($con,"insert into appointment(doctorSpecialization,doctorId,
                                                         
                                                         <button type="submit" name="submit" class="btn btn-o btn-primary">
                                                             Submit
-                                                        </button>
+                                                        </button>                                                            
+                                                                                                            </form>
+                                                                                                        </div>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                                    
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                            
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    
+                                                                                 
+                                                                    
+                                                                            
+                                                                            
+                                                                                
+                                                                                
+                                                                            
+                                                                                 
+                                                                                
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                        
+                                                                </div>
+
+                                                                
+                                                                        <!-- start: MAIN JAVASCRIPTS -->
+                                                                        <script src="vendor/jquery/jquery.min.js"></script>
+                                                                        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+                                                                        <script src="vendor/modernizr/modernizr.js"></script>
+                                                                        <script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+                                                                        <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+                                                                        <script src="vendor/switchery/switchery.min.js"></script>
+                                                                        <!-- end: MAIN JAVASCRIPTS -->
+                                                                        <!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+                                                                        <script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+                                                                        <script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+                                                                        <script src="vendor/autosize/autosize.min.js"></script>
+                                                                        <script src="vendor/selectFx/classie.js"></script>
+                                                                        <script src="vendor/selectFx/selectFx.js"></script>
+                                                                        <script src="vendor/select2/select2.min.js"></script>
+                                                                        <script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+                                                                        <script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+                                                                        <!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+                                                                        <!-- start: CLIP-TWO JAVASCRIPTS -->
+                                                                        <script src="assets/js/main.js"></script>
+                                                                        <!-- start: JavaScript Event Handlers for this page -->
+                                                                        <script src="assets/js/form-elements.js"></script>
+                                                                        <script>
+                                                                            jQuery(document).ready(function() {
+                                                                                Main.init();
+                                                                                FormElements.init();
+                                                                            });
+                                                                
+                                                                            $('.datepicker').datepicker({
+                                                                    format: 'yyyy-mm-dd',
+                                                                    startDate: '-3d'
+                                                                });
+                                                                        </script>
+                                                                          <script type="text/javascript">
+                                                                            $('#timepicker1').timepicker();
+                                                                        </script>
+                                                                        <!-- end: JavaScript Event Handlers for this page -->
+                                                                        <!-- end: CLIP-TWO JAVASCRIPTS -->
+                                                                
+                                                                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+                                                                
+                                                                    </body>
+                                                                </html>
